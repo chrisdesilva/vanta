0e0b00d3137b4a1ebde121c6e5f2cd2f055705c7
investng.eth <93827718+topherchris420@users.noreply.github.com>
2022-10-28 23:53:17 -0400

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -26,7 +26,7 @@ export default function Home() {
   return (
     <div className={styles.container}>
       <Head>
-        <title>👫👫👫👫.eth</title>
+        <title>🚀christopher</title>
         <link rel="icon" href="%PUBLIC_URL%/favicon.ico" />
       </Head>
 
